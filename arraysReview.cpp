aced95fcb5d2d79c45801d131e91664b7600ea78
benjas909 <benjamin.aguilerac@usm.cl>
2022-05-29 01:16:13 -0400

    a veces mi cerebro no quiere cooperar

--- arraysReview.cpp
+++ arraysReview.cpp
@@ -4,26 +4,26 @@ using namespace std;
 
 int main(){
 
-    //declaring a list of integers and assigning values
-    int tinylist[] = {1, 3, 4 ,2};
+    //declaring an array of integers and assigning values
+    int tinyarray[] = {1, 3, 4 ,2};
 
-    //accessing and printing a value from the list
-    cout << tinylist[1] << endl;
+    //accessing and printing a value from the array
+    cout << tinyarray[1] << endl;
 
-    //modifying a value from the list
-    tinylist[1] = 6;
-    cout << tinylist[1] << endl;
+    //modifying a value from the array
+    tinyarray[1] = 6;
+    cout << tinyarray[1] << endl;
     
-    //looping through the elements of a list
+    //looping through the elements of an array
     for (int i = 0; i < 4; i++){
-        cout << tinylist[i] << endl;
+        cout << tinyarray[i] << endl;
     }
 
-    //a list can also be declared without specifying the number of elements
+    //an array can also be declared without specifying the number of elements
     string names[] = {"Benja", "Bingus"};
     //but it will only reserve space for the number of elements assigned in its declaration, in this case, 2
 
-    //you can also declare a list without assigning any values initially, reserving the spaces in memory to assign later
+    //you can also declare an array without assigning any values initially, reserving the spaces in memory to assign later
     int nums[4];
     nums[0] = 4;
     nums[1] = 3;
