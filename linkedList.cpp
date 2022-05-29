8c87f965fcf7d7be61d745cf7a9e1357da5e57b0
benjas909 <benjamin.aguilerac@usm.cl>
2022-05-29 19:56:54 -0400

    esa lista aún no estaba lista

--- linkedList.cpp
+++ linkedList.cpp
@@ -35,6 +35,49 @@ class tLista {
             listSize++;
             return pos;
         }
+        void erase() {
+            tNodo* aux = curr->sig;
+            curr->sig = curr->sig->sig;
+            delete aux;
+        }
+        void moveToStart() {
+            curr = head;
+            pos = 0;
+        }
+        void moveToEnd() {
+            curr = tail;
+            pos = listSize;
+        }
+        void prev() {
+            tNodo* temp;
+            if (curr == head) {
+                return;
+            }
+            temp = head;
+            while (temp->sig != curr) {
+                temp = temp->sig; 
+            }
+            curr = temp;
+            pos--;
+        }
+        void next() {
+            if (curr != tail) {
+                curr = curr->sig;
+                pos++;
+            }
+        }
+        void moveToPos(unsigned int posicion){
+            unsigned int i;
+            if (posicion < 0 || posicion > listSize) {
+                return;
+            }
+            curr = head;
+            pos = 0;
+            for (i = 0; i < posicion; i++) {
+                curr = curr->sig;
+                pos++;
+            }
+        }
 
 
 };
