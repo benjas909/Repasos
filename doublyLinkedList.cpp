65e8a29e5ae8a45e2dee91e457d549a84c80db4d
benjas909 <benjamin.aguilerac@usm.cl>
2022-06-06 00:49:13 -0400

    enlazando enlaces

---
+++ doublyLinkedList.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+using namespace std;
+
+typedef int tElemLista;
+
+struct tNodo {
+    tElemLista info;
+    tNodo* sig;
+    tNodo* ant;
+};
+
+class tLista {
+    private:
+        tNodo* head;
+        tNodo* tail;
+        tNodo* curr;
+        unsigned int listSize;
+        unsigned int pos;
+    public:
+        tLista() {
+            head = curr = new tNodo;
+            tail = new tNodo;
+            head->sig = tail;
+            tail->ant = head;
+            listSize = 0;
+            pos = 0;
+        }
+        void clear() {
+            tNodo* ptrh = head;
+            tNodo* ptrt = tail;
+            tNodo* temp1;
+            tNodo* temp2;
+            while(ptrh != NULL && ptrt != NULL && ptrh->sig != ptrt && ptrt->ant != ptrh) {
+                temp1 = ptrh->sig;
+                temp2 = ptrt->ant;
+                delete ptrh; listSize--;
+                delete ptrt; listSize--;
+                ptrh = temp1;
+                ptrt = temp2;
+            }
+        }
+        
+        // int insertFront(tElemLista item) {
+        //     tNodo* newNode = new tNodo;
+        //     newNode->info = item;
+        //     newNode->sig = head;
+        //     newNode->ant = NULL;
+
+        //     if(head != NULL)
+        //         head->ant = newNode;
+
+        //     head = newNode; 
+            
+
+        // }
+
+        int insert(tElemLista item) {
+            tNodo* aux = curr->sig;
+            curr->sig = curr->sig->ant = new tNodo;
+            curr->sig->info = item;
+            curr->sig->sig = aux;
+            curr->sig->ant = curr;
+            if (curr == tail) {
+                tail = curr->sig;
+            }
+            listSize++;
+            return pos;
+        }
+        void erase() {
+            tNodo* aux = curr->sig;
+            curr->sig = curr->sig->sig;
+            delete aux;
+            listSize--;
+        }
+        void moveToStart() {
+            curr = head;
+            pos = 0;
+        }
+        void moveToEnd() {
+            curr = tail;
+            pos = listSize;
+        }
+        void prev() {
+            tNodo* temp;
+            if (curr == head) {
+                curr = curr->ant;
+            }
+            pos--;
+        }
+        void next() {
+            if (curr != tail) {
+                curr = curr->sig;
+                pos++;
+            }
+        }
+        void moveToPos(unsigned int posicion){
+            unsigned int i;
+            if (posicion < 0 || posicion > listSize) {
+                return;
+            }
+            curr = head;
+            pos = 0;
+            for (i = 0; i < posicion; i++) {
+                curr = curr->sig;
+                pos++;
+            }
+        }
+        void printItem(){
+            if (curr == NULL && head == NULL && tail == NULL) cout << "lista vacía";
+            cout << curr->info;
+        }
+        int length() {
+            return listSize;
+        }
+
+
+};
+
+int main() {
+    tLista test_list;
+    tElemLista cosa;
+    int i, wea;
+    cout << "cantidad de cosas: "; 
+    cin >> i;
+
+    for (int j = 0; j < i; j++) {
+        cout << "cosa a insertar: ";
+        cin >> wea;
+        test_list.insert(wea);
+        test_list.next();
+    }
+    test_list.moveToStart();
+    for (int k = 0; k < i; k++){
+        test_list.next();
+        test_list.printItem();
+        cout << "\n";
+    }
+    test_list.clear();
+    test_list.moveToStart();
+    cout << test_list.length();
+    // test_list.printItem();
+
+
+
+    return 0;
+}
